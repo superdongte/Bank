f8e062cfb68fbdd1f71e10f9d3214d6597cdc114
superdongte <superdongte@naver.com>
2022-01-04 12:18:54 +0900

    teest2 생성

--- src/main/java/com/example/bank/controller/HomeController.java
+++ src/main/java/com/example/bank/controller/HomeController.java
@@ -13,6 +13,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
+//	@GetMapping("/")
+//	public String test(Model model) throws IOException {
+//		String WeatherURL = "https://weather.naver.com/today";
+//		Document doc = Jsoup.connect(WeatherURL).get();
+//		//HTML로 부터 데이터 가져오기
+//		Elements elem = doc.select(".weather_area .summary .weather");
+//		//원하는 태그 선택
+//		String[] str = elem.text().split(" ");
+//		//정보파싱
+//		model.addAttribute("weather",elem);
+//	
+//		System.out.println("갔는지 보는중"+elem);
+//		return "test";
+//	}
 	@GetMapping("/")
 	public String test(Model model) throws IOException {
 		String WeatherURL = "https://weather.naver.com/today";
@@ -25,6 +39,7 @@ public class HomeController {
 		model.addAttribute("weather",elem);
 	
 		System.out.println("갔는지 보는중"+elem);
-		return "test";
+		return "test2";
 	}
+	
 }
